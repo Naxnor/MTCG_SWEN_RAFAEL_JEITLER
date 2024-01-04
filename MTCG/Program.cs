35be72d5fec6b5a33b50e0c6824f7039d83ce3fb
Rafael Jeitler <116429608+Naxnor@users.noreply.github.com>
2024-01-04 15:40:23 +0100

    initial

---
+++ MTCG/Program.cs
@@ -0,0 +1,33 @@
+﻿using MTCG.Server;
+
+namespace MTCG
+{
+    internal class Program
+    {
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // entry point                                                                                                      //
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        
+        /// <summary>Main entry point.</summary>
+        /// <param name="args">Arguments.</param>
+        static void Main(string[] args)
+        {
+            HttpSvr svr = new();
+            svr.Incoming += _ProcessMessage;
+
+            svr.Run();
+        }
+
+
+        /// <summary>Event handler for incoming server requests.</summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="e">Event arguments.</param>
+        private static void _ProcessMessage(object sender, HttpSvrEventArgs e)
+        {
+            Console.WriteLine(e.PlainMessage);
+
+            e.Reply(200, "Yo! Understood.");
+            
+        }
+    }
+}
